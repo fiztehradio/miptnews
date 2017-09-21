c3a0081c4f8681e3e66fe89c49e24d43352726b5
Okhlopkov Daniil Olegovich <okhlopkov@phystech.edu>
2017-09-21 16:35:33 +0300

    move create_table to load_and_post

--- load_and_post.py
+++ load_and_post.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 #-*- coding: UTF 8 -*-
 
+import os
+import sqlite3
 from objs.bot import ExportBot
+
+database = 'miptnews.db'
+
+if not os.path.isfile(database):
+    db = sqlite3.connect(database)
+    c = db.cursor()
+    c.execute('CREATE TABLE miptnews (id INTEGER PRIMARY KEY, text VARCHAR(200), link VARCHAR(100), date UNIX_TIME, publish UNIX_TIME, chat_id INTEGER, message_id INTEGER)')
+
 bot = ExportBot()
 bot.detect()
 bot.public_posts()
